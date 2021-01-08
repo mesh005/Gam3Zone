b4ad44b1c19caea374016ebba48e3825770e6e29
mesh005 <mesherlive@msn.com>
2021-01-08 21:48:56 +0000

    navigation setup

---
+++ app/src/main/java/com/example/gam3zone/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gam3zone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configure toolbar
+        Toolbar myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+
+
+        //Configure bottomNavBar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+    }
+}
---
+++ app/src/main/java/com/example/gam3zone/PagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.gam3zone;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.gam3zone.NintendoFragment;
+import com.example.gam3zone.PlaystationFragment;
+import com.example.gam3zone.XboxFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class PagerAdapter extends FragmentPagerAdapter {
+
+//    private int numOfTabs;
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> titleList = new ArrayList<>();
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        titleList.add(title);
+    }
+}
